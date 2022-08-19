b96b362bf7660eaa47b18676e0795341d2f0bc42
Luis Paz <lpaz8806@gmail.com>
2022-08-19 06:49:43 +0200

    skeleton

---
+++ src/main/java/se/jensensthlm/hellogithubactions/HelloGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package se.jensensthlm.hellogithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGithubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/jensensthlm/hellogithubactions/HelloGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensensthlm.hellogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
